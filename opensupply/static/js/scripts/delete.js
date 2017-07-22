122088a678a1c1554a14d967dbf98cbc09c9898d
etoko <emmanuel.toko@gmail.com>
2017-07-22 18:30:15 +0300

    delete confirmation: yet to be implemented

---
+++ opensupply/static/js/scripts/delete.js
@@ -0,0 +1,26 @@
+function delete_operation()
+{
+  alert("Delete Operation");
+
+            //var Id = dijit.byId("_Id").getValue();
+        
+            //if (Id.toString() == "NaN")
+           // {
+           //     dojo.byId("InformationMessage").innerHTML = "You can only delete a viewable ";
+           //     dijit.byId("InformationMessageDialog").show();
+           //     return;
+           // }
+        
+          // var Status = dojo.byId("Status").innerHTML;
+        
+       //     if (Status.toString() !== "PENDING")
+         //   {
+           //   var message = dojo.byId("InformationMessage");
+            //  message.innerHTML = "You cannot edit a purchase order for which at least one Goods Received Note has already been received";
+              //dijit.byId("InformationMessageDialog").show();
+        
+             // return;
+            }//ENd of if statement block
+        
+            //dijit.byId("DeleteDialog").show();
+}
